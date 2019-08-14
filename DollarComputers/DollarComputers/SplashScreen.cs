0f2bd6fe1442bce6ba65d4bd9ea415aa81e9bf06
taran001 <amankaramjit@hotmail.com>
2019-08-14 05:14:08 -0700

    Splash Screen and Start Form

---
+++ DollarComputers/DollarComputers/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DollarComputers
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            //TODO: Add Icon
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(3000);
+
+        }
+    }
+}
---
+++ DollarComputers/DollarComputers/StartForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DollarComputers
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartNewOrderButton = new System.Windows.Forms.Button();
+            this.OpenSavedOrderButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartNewOrderButton
+            // 
+            this.StartNewOrderButton.Location = new System.Drawing.Point(90, 168);
+            this.StartNewOrderButton.Name = "StartNewOrderButton";
+            this.StartNewOrderButton.Size = new System.Drawing.Size(117, 23);
+            this.StartNewOrderButton.TabIndex = 1;
+            this.StartNewOrderButton.Text = "Start New Order";
+            this.StartNewOrderButton.UseVisualStyleBackColor = true;
+            this.StartNewOrderButton.Click += new System.EventHandler(this.StartNewOrderButton_Click);
+            // 
+            // OpenSavedOrderButton
+            // 
+            this.OpenSavedOrderButton.Location = new System.Drawing.Point(90, 197);
+            this.OpenSavedOrderButton.Name = "OpenSavedOrderButton";
+            this.OpenSavedOrderButton.Size = new System.Drawing.Size(117, 23);
+            this.OpenSavedOrderButton.TabIndex = 2;
+            this.OpenSavedOrderButton.Text = "Open a Saved Order";
+            this.OpenSavedOrderButton.UseVisualStyleBackColor = true;
+            this.OpenSavedOrderButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DollarComputers.Properties.Resources.Standing_Man_96px;
+            this.pictureBox1.Location = new System.Drawing.Point(74, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.OpenSavedOrderButton);
+            this.Controls.Add(this.StartNewOrderButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome To Dollar Computers";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button StartNewOrderButton;
+        private System.Windows.Forms.Button OpenSavedOrderButton;
+        private System.Windows.Forms.Button button3;
+    }
+}
